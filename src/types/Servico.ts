7a7f141fab0cb9d7f3b39493b3eccfc096df5a63
ggustavo-souza <cavalogames1231@gmail.com>
2026-09-23 16:56:51 -0300

    chore: desinstalando o google maps API, instalando o leaflet e alterando o funcionamento do type do serviço

--- src/types/Servico.ts
+++ src/types/Servico.ts
@@ -2,5 +2,5 @@ export type MarcadorServico = {
     id: number;
     situacao: "pendente" | "concluida" | "negada";
     categoria: "poda" | "erradicacao" | "plantio" | "rocagem"
-    coordenadas: { lat: number; lng: number };
+    coordenadas: [number, number];
 };
